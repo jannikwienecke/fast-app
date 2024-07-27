6d0e083e19cde51a175c7bde6bf071faa8af53ea
jannikwienecke <wienecke.jannik@gmail.com>
2024-07-27 20:45:42 +0200

    add gh action and playwright

---
+++ e2e/example.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByText(/You clicked 0 times/)).toBeVisible();
+
+  await page.click("text=Click me");
+
+  await expect(page.getByText(/You clicked 1 times/)).toBeVisible();
+});
