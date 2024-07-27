6cc7c512b8b6c91cb01c40c024c5f5ebb2ca05ef
jannikwienecke <wienecke.jannik@gmail.com>
2024-07-27 21:12:39 +0200

    add shadcn components --all

--- app/client.tsx
+++ app/client.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Slider } from "@/components/ui/slider";
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export const Client = () => {
@@ -10,9 +14,31 @@ export const Client = () => {
   });
 
   return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      <p>You clicked {count} times</p>
+    <div className="flex flex-col space-y-2 justify-center items-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Counter</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col space-y-2">
+          <p className="flex flex-row items-center justify-center">
+            You clicked{" "}
+            <span className="min-w-[35px] grid place-items-center font-bold">
+              {count}
+            </span>{" "}
+            times
+          </p>
+
+          <Button onClick={() => setCount(count + 1)}>Click me</Button>
+
+          <Slider
+            value={[count]}
+            onValueChange={(values) => setCount(values[0])}
+            max={100}
+            step={1}
+            className={cn("w-[60%] pt-4")}
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 };
