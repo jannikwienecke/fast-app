244018e7c081a4ba30f1d48783a59d5ef4f083e3
jannikwienecke <wienecke.jannik@gmail.com>
2024-07-27 21:21:10 +0200

    add prettierrc and eslintrc

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import Image from "next/image";
-import { Client } from "./client";
+import { Client } from "./client"
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export default async function Home() {
-  const x = await prisma.user.findMany();
-
-  console.log(x);
-
+  const x = await prisma.user.findMany()
+  console.log(x)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Client />
     </main>
-  );
+  )
 }
