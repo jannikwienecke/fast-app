c2938d9f15841e35ff11319156d25662334f753e
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-07-28 09:55:46 +0200

    add query user for testing db in vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { Client } from "./client"
+import { prisma } from "./db"
 
 export default async function Home() {
+  const user = await prisma.user.findFirst()
+  console.log({ user })
+
   return (
     <main className="min-h-screen grid place-items-center">
       <Client />
