8f0426225b2ee1aca7d7e35d0f293469f501aac5
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-07-28 10:26:04 +0200

    remove .env

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import { Client } from "./client"
 import { prisma } from "./db"
 
 export default async function Home() {
-  const user = await prisma.user.findFirst()
-  console.log({ user })
+  const user = await prisma.user.findMany()
 
   return (
     <main className="min-h-screen grid place-items-center">
       <div>
-        <div>First User::: {user?.name ?? "No user"}</div>
+        {user?.map((user) => (
+          <div key={user.id}>
+            <div>User::: {user.name ?? "No user"}</div>
+          </div>
+        ))}
+
         <Client />
       </div>
     </main>
