f5c30d395e54b40a93363defa5119de883e4fdcf
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-07-28 10:30:09 +0200

    add force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Client } from "./client"
 import { prisma } from "./db"
 
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
   const user = await prisma.user.findMany()
 
